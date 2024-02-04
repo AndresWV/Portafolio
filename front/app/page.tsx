e8f28f317b9b32a22031c2adeeb5309172c5f8c7
AndresWV <andres.wv@hotmail.com>
2024-02-04 19:35:37 -0300

    feat(NavBar.tsx): add navbar component with design 1.0

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,9 @@
 import styles from "./page.module.css";
-
+import NavBar from "./components/NavBar";
 export default function Home() {
   return (
     <div className={styles.main}>
-     
+      <NavBar />
     </div>
   );
 }
