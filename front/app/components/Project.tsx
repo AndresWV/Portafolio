ef3be21777a434cfd2deff1e530da88abd092eb4
Andrés Wallberg <andres.wv@hotmail.com>
2024-05-19 21:29:52 -0400

    project card base

---
+++ front/app/components/Project.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import Modal from '@mui/material/Modal';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+interface ProjectProps {
+  open: boolean;
+  onClose: () => void;
+  icon: string;
+  title: string;
+  description: string;
+  projectLink: string;
+  technologies: string[];
+}
+const Project: React.FC<ProjectProps> = ({ open, onClose, icon, title, description, projectLink, technologies }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Grid
+        container
+        sx={{
+          backgroundColor: '#2D2D35',
+          boxShadow: 5,
+          padding: 4,
+          borderRadius: 8,
+          outline: 'none',
+          minWidth: 300,
+          maxWidth: 600,
+          position: 'relative',
+        }}
+      >
+        <IconButton aria-label="close" onClick={onClose} sx={{ position: 'absolute', top: 20, right: 20,color: '#b3b3b4 !important'}}>
+          <CloseIcon />
+        </IconButton>
+        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', marginBottom: 2 }}>
+          <img src={icon} alt="Project Icon" style={{ borderRadius: '50%', padding: 2 }} />
+        </Grid>
+        <Grid item xs={12} sx={{ marginTop: 2 }}>
+          <Typography variant="h2" align="center" sx={{fontFamily: 'IBM Plex Mono'}}>
+            {title}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ marginTop: '5%' }}>
+          <Typography variant="body1" align="center" sx={{ color: '#FFFFFF', fontFamily: 'IBM Plex Mono' }}>
+            {description}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ marginTop: '5%' }}>
+          <Typography variant="h4" align="center" component="a" href={projectLink} target="_blank" rel="noopener noreferrer" 
+            sx={{ 
+              color: '#64B6AC', 
+              textDecoration: 'none', 
+              '&:hover': { textDecoration: 'underline' },
+              fontFamily: 'IBM Plex Mono'
+              }}>
+            Project Link
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ marginTop: '5%', display: 'flex', justifyContent: 'center', gap: 1 }}>
+          {technologies.map((tech, index) => (
+            <Typography key={index} variant="body2" color="textSecondary">
+              {tech}
+            </Typography>
+          ))}
+        </Grid>
+      </Grid>
+    </Modal>
+  );
+}
+export default Project;
--- front/app/components/Projects.tsx
+++ front/app/components/Projects.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { Card, CardContent, CardMedia } from '@mui/material';
+import Project from './Project';
+
+interface ProjectData {
+  img: string;
+  title: string;
+  description: string;
+  technologies: string[];
+}
 
 export default function Projects() {
-  const projects = [
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState<ProjectData | null>(null);
+
+  const projects: ProjectData[] = [
     {
       img: '/assets/img/icoProjects.jpg',
       title: 'Project 1',
@@ -20,6 +32,11 @@ export default function Projects() {
     // Agrega más proyectos según sea necesario
   ];
 
+  const handleProjectClick = (project: ProjectData) => {
+    setSelectedProject(project);
+    setModalOpen(true);
+  };
+
   return (
     <Grid id="projects" container sx={{ paddingX: '15%', marginTop: '7%' }}>
       <Grid xs={12} sx={{ display: 'flex' }}>
@@ -27,18 +44,21 @@ export default function Projects() {
       </Grid>
       {projects.map((project, index) => (
         <Grid key={index} xs={12} sx={{ display: 'flex', marginTop: '2%', justifyContent: 'center' }}>
-          <Card sx={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            paddingY: '2%',
-            paddingX: '2%',
-            backgroundColor: '#2D2D35',
-            borderRadius: '5px',
-            maxWidth: '100%',
-            width: '100%',
-            display: 'flex',
-            flexDirection: { xs: 'column', sm: 'row' },
-          }}>
+          <Card
+            onClick={() => handleProjectClick(project)}
+            sx={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingY: '2%',
+              paddingX: '2%',
+              backgroundColor: '#2D2D35',
+              borderRadius: '5px',
+              maxWidth: '100%',
+              width: '100%',
+              display: 'flex',
+              flexDirection: { xs: 'column', sm: 'row' },
+            }}
+          >
             <CardMedia
               component="img"
               height="auto"
@@ -52,7 +72,7 @@ export default function Projects() {
               }}
             />
 
-            <CardContent sx={{ flex: '1', marginLeft:'3%' }}>
+            <CardContent sx={{ flex: '1', marginLeft: '3%' }}>
               <Typography variant="h4" sx={{ color: '#b3b3b4', fontFamily: 'IBM Plex Mono', marginBottom: '10px' }}>
                 {project.title}
               </Typography>
@@ -75,6 +95,17 @@ export default function Projects() {
           </Card>
         </Grid>
       ))}
+      {selectedProject && (
+        <Project
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
+          icon={selectedProject.img}
+          title={selectedProject.title}
+          description={selectedProject.description}
+          projectLink="https://example.com"
+          technologies={selectedProject.technologies}
+        />
+      )}
     </Grid>
   );
 }
